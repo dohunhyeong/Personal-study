8ae627965e9402bcf90e1396b54e075ffd890671
dohunhyeong <hdhd4422@naver.com>
2026-02-03 14:46:45 +0900

    tag updated

--- my-react-app/src/basic/Header.jsx
+++ my-react-app/src/basic/Header.jsx
@@ -1,1 +1,17 @@
-function Header
+function Header(){
+    return(
+        <>
+        <h1>My website</h1>
+        <nav>
+            <ul>
+                <li>
+                    <a href ="#">Home</a>
+                </li>
+                <li><a href = "#">About</a></li>
+            </ul>
+        </nav>
+        </>
+    );
+}
+
+export default Header;
